cc414c60ee36d38be9ed4a56e606268f451189c9
KacperW18 <161600887+KacperW18@users.noreply.github.com>
2024-03-13 09:31:45 +0000

    Excercise 1 - 12

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -4,8 +4,8 @@ Tatyana = int(input())
 output = ""
 if alex_age > Tatyana:
     output = "Alex is the eldest."
-if Tatyana > alex_age:
+elif Tatyana > alex_age:
     output = "Tatyana is the eldest."
 else:
-    output = "Alex and Tatyana are the same age."
+    output = "Alex and Tatyana are of the same age."
 print(output)
