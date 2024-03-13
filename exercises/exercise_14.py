3f69afda054e1642460516f0e96f80c7b12a5f31
KacperW18 <161600887+KacperW18@users.noreply.github.com>
2024-03-13 10:37:35 +0000

    all except 18

--- exercises/exercise_14.py
+++ exercises/exercise_14.py
@@ -1,2 +1,6 @@
 # Your solution to Exercise 14
-
+number = input()
+if number[0:2] == number[4:1:-1]:
+    print("True")
+else:
+    print("False")
--- exercises/exercise_17.py
+++ exercises/exercise_17.py
@@ -1,2 +1,12 @@
 # Your solution to Exercise 17
-
+number = int(input())
+num1 = number // 100000
+num2 = number // 10000 % 10
+num3 = number // 1000 % 10
+num4 = number // 100 % 10
+num5 = number // 10 % 10
+num6 = number % 10
+if num1 + num2 + num3 == num4 + num5 + num6:
+    print("Happy")
+else:
+    print("Ordinary")
